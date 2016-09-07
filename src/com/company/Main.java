53841a6aca73d3c606621d09bbe13f498d3943f8
Jiayi Yin <yinjiayi5@gmail.com>
2016-09-07 18:59:23 -0400

    Lesson 1 part 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,20 @@ package com.company;
 
 public class Main {
 
+    /*Author: Jiayi Yin
+    Date created: September 7, 2016
+    Public class tester*/
+
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("From: Bill Smith");
+        System.out.println("Address: Dell Computer, Blgd 13");
+        System.out.println("Date: April 12, 2005");
+
+        System.out.println("To: Jack Jones");
+
+        System.out.println("Message: Help! I'm trapped inside a computer!");
+
+
     }
 }
